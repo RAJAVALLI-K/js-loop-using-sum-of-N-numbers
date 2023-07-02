cd0d0e45135aace072fa76613ea5dea60de6ed89
RAJAVALLI-K <rajavalli1932bca@gmail.com>
2023-07-02 12:01:52 +0530

    add sum of n numbers

---
+++ main.js
@@ -0,0 +1,9 @@
+// 2-Sum of N natrural numbers
+
+let value = parseInt(prompt("Enter a number:"));
+let sum = 0;
+for (let i = 0; i < value; i++) {
+    sum = sum + i;
+    // console.log("sum numbers:" + sum);
+}
+console.log("sum numbers:" + sum);
